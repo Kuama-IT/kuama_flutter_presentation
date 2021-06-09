1573c531ad051876a6f578d4b1676b6b92ece3c3
SimoneBressan <simone@kuama.it>
2021-06-09 18:03:02 +0200

    PositionerBloc - Allows you to track the user's location - Allows you to retrieve the user's current position

--- lib/permissions.dart
+++ lib/permissions.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-library kuama_presentation_feature_permissions;
+library kuama_presentation_permissions;
 
 export 'src/features/permissions/bloc/permission_bloc.dart';
 export 'src/features/permissions/widgets/ask_permission_button_bloc_builder.dart';
---
+++ lib/positioner.dart
@@ -0,0 +1,9 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+library kuama_presentation_positioner;
+
+export 'src/features/positioner/bloc/positioner_bloc.dart';
+export 'src/features/positioner/widgets/ask_position_service_dialog.dart';
+export 'src/features/positioner/widgets/localize_position_bloc_listener.dart';
+export 'src/features/positioner/widgets/position_button_bloc_builder.dart';
+export 'src/features/positioner/widgets/position_view_bloc_builder.dart';
